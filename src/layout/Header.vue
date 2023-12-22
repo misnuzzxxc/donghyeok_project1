cd8e6fa0c3a71fc330e057337a2bc5d2e6fae3d5
donghyeokkim <kimki131702@naver.com>
2023-12-22 18:09:56 +0900

    vuejs 게시판 등 다양한 공부한것들

---
+++ src/layout/Header.vue
@@ -0,0 +1,55 @@
+<template>
+	<header>
+		<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">LOGO</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto">
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/"
+								>Home</RouterLink
+							>
+						</li>
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/about"
+								>About</RouterLink
+							>
+						</li>
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/posts"
+								>게시판</RouterLink
+							>
+						</li>
+					</ul>
+					<div class="d-flex">
+						<button class="btn btn-outline-light" type="button" @click="goPage">
+							작성
+						</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goPage = () => {
+	router.push('/posts/create');
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/posts/PostDetailVeiw.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h2>게시글 상세</h2>
+		{{ $route.params }}
+	</div>
+	<hr class="my-4" />
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
